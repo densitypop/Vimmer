5735c8cbac5545d2534b85960fe2d675ea9b7130
Joe Fiorini <joe@faithfulgeek.org>
2010-12-19 13:51:21 -0500

    Upgrade to latest testing libraries

--- features/support/setup.rb
+++ features/support/setup.rb
@@ -1,4 +1,4 @@
-require 'aruba'
+require 'aruba/cucumber'
 
 ENV['RUBYOPT'] = "-r#{File.expand_path(File.join(File.dirname(__FILE__), 'stub-commands.rb'))} #{ENV['RUBYOPT']}"
 ENV['VIMMER_HOME'] = File.expand_path(File.join(File.dirname(__FILE__),
